805bdcada7f82f0fa886e72073e7fa06a03ef4ab
crislo <krzysztof.logaciuk@gmail.com>
2021-12-07 11:51:44 +0100

    feat: repository initialized

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# doEdu-backend
+## Installation
+
+``` Create .env file in main directory and set PostgreSQL database connection variables like:
+POSTGRES_HOST=127.0.0.1
+POSTGRES_PORT=5432 or 5433
+POSTGRES_USER=username
+POSTGRES_PASSWORD=userpassword
+POSTGRES_DATABASE=database_name
+
+```
+
+```bash
+$ npm install
+```
+
+## Running the app
+
+```bash
+# development
+$ npm run start
+
+# watch mode
+$ npm run start:dev
+
+# production mode
+$ npm run start:prod
+```
+
---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(<string>process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true // shouldn't be used in production - may lose data
+    })
+  ],
+  controllers: [AppController],
+  providers: [AppService]
+})
+export class AppModule {}
